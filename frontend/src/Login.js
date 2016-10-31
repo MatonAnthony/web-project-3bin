c18ec957fc1dd60d84878f5cd2cbbe7a185b2df2
Anthony Maton <maitre.negi@gmail.com>
2016-10-31 16:02:30 +0100

    Implement a login form

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,68 +1,75 @@
-import React, { Component } from 'react';
-import { Panel, FormGroup, ControlLabel, FormControl } from 'react-bootstrap';
+import React from 'react';
+import { Panel, FormGroup, ControlLabel, FormControl, Button } from 'react-bootstrap';
+import './Login.css';
 
-class Login extends Component {
+const Login = React.createClass({
     getInitialState() {
-      return {
-          login: 'pseudo',
-          password: 'password'
-      };
-    }
+        return {
+            login: 'pseudo',
+            password: 'password'
+        };
+    },
 
     getValidationState() {
         const login = this.state.login;
         const password = this.state.password;
 
-        if(login.length <= 0 || password.length <= 0) return 'error';
-        return 'success';
-    }
+        if(login.length <= 0 || password.length <= 0) return "error";
+        return "success";
+    },
 
     handleLoginChange(event) {
-        this.setState({ login : event.target.value }, () => {
-            this.getValidationState();
-        });
-    }
+    this.setState({ login : event.target.value }, () => {
+        this.getValidationState();
+    });
+},
 
     handlePasswordChange(event) {
-        this.setState({ password : event.target.value }, () => {
-            this.getValidationState();
-        });
-    }
+    this.setState({ password : event.target.value }, () => {
+        this.getValidationState();
+    });
+},
 
     render() {
-        const title = "Login";
-        const panelColor = "primary"; // Please refer to bootstrap color notation
+        let title = "Login";
+        let panelColor = "primary"; // Please refer to bootstrap color notation*/
         return (
-            <div>
-                <Panel header={title} bsStyle={panelColor}>
-                    <form>
-                        <FormGroup
-                            controlID="formLogin"
-                            validationState={this.getValidationState}
-                        >
-                            <ControlLabel>Username :</ControlLabel>
-                            <FormControl
-                                type="text"
-                                value={this.state.login}
-                                placeholder={this.state.login}
-                                onChange={this.handleLoginChange}
-                            />
-                            <FormControl.Feedback />
+            <div className="container center-box vertical-centering">
+                <div>
+                    <Panel header={title} bsStyle={panelColor}>
+                        <form>
+                            <FormGroup
+                                validationState={this.getValidationState()}
+                            >
+                                <ControlLabel>Username :</ControlLabel>
+                                <FormControl
+                                    type="text"
+                                    value={this.state.login}
+                                    placeholder={this.state.login}
+                                    onChange={this.handleLoginChange}
+                                />
+                                <FormControl.Feedback />
 
-                            <ControlLabel>Password :</ControlLabel>
-                            <FormControl
-                                type="password"
-                                value={this.state.password}
-                                placeholder={this.state.password}
-                                onChange={this.handlePasswordChange}
-                            />
-                            <FormControl.Feedback />
-                        </FormGroup>
-                    </form>
-                </Panel>
+                                <ControlLabel>Password :</ControlLabel>
+                                <FormControl
+                                    type="password"
+                                    value={this.state.password}
+                                    placeholder={this.state.password}
+                                    onChange={this.handlePasswordChange}
+                                />
+                                <FormControl.Feedback />
+                            </FormGroup>
+
+                            <Button
+                                type="submit"
+                                bsStyle="success"
+                            > Submit </Button>
+                        </form>
+                    </Panel>
+                </div>
             </div>
         );
     }
-}
+});
 
 export default Login;
