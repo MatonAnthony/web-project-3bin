d809cc6b70394e32ae1b6a4ddf389a5234915fe9
Techy Martin <Delounea@users.noreply.github.com>
2016-11-04 16:09:27 +0100

    Faker ticket payment (#3)
    * Add model for Payment and Ticket.
    
    * codestyle fix
    
    * codestyle fix
    
    * codestyle fix

--- data-generator/users.js
+++ data-generator/users.js
@@ -1,7 +1,7 @@
 /*
  * Generate a set of fake user used by our API mocker (json-server)
  */
-let faker = require('faker');
+const faker = require('faker');
 /*
  * Ask faker to give us a french like dataset
  */
@@ -14,7 +14,7 @@ faker.locale = 'fr';
  */
 function generateUsers(numberOfUsers) {
     let users = [];
-    for(let i = 0; i < numberOfUsers; i++) {
+    for (let i = 0; i < numberOfUsers; i++) {
         let pseudo = faker.internet.userName();
         let password = faker.internet.password();
         let email = faker.internet.email();
