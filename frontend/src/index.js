1b91df8d5a9eb7e01f80d5c16152a54ba5b6bb5e
Maton Anthony <MatonAnthony@users.noreply.github.com>
2016-11-12 15:54:37 +0100

    Frontend react + Login page (#11)
    * frontend bootstrap using ReactJS
    
    * Implement a login form
    
    * Create the POST method to submit the form (still lacking a mock)
    
    * Replace CSS hack by the removal of FormControl.feedback
    
    * Replace type=submit by type=button on submit button
    
    * Fixed vertical alignment issue
    
    * Improve .gitignore content
    
    * Removal of unecessary config files
    
    * Fix eslint issues
    
    Add various eslint component to fully support React and JSX specific rules.
    
    * Refactor .gitignore + Application of new gitignore

---
+++ frontend/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import Login from './Login';
+import './index.css';
+
+let isLogged = false;
+if(isLogged) {
+    ReactDOM.render(
+        <App />,
+        document.getElementById('root')
+    );
+} else {
+    ReactDOM.render(
+        <Login />,
+        document.getElementById('root')
+    );
+}
