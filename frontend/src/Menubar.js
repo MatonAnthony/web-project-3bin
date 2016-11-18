618d905be679f74db07adfee5fe7c53046746b7b
Anthony Maton <maitre.negi@gmail.com>
2016-11-18 15:07:56 +0100

    Menubar Component

---
+++ frontend/src/Menubar.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import {Navbar, Nav, Button} from 'react-bootstrap';
+import './Menubar.css';
+
+const moment = require('moment');
+
+const Menubar = React.createClass({
+    getDefaultProps() {
+        return {
+            date: moment().format('LLL'),
+        };
+    },
+
+    getInitialState() {
+        return {
+            username: undefined,
+            clock: moment().format('LLL'),
+        };
+    },
+
+    componentDidMount() {
+        window.setInterval(() => {
+            this.setState({
+                clock: moment().format('LLL'),
+            });
+        }, 5000);
+    },
+
+    handleLogout() {
+        /*
+         * TODO: Define how logout is going to be handled
+         */
+    },
+
+    render() {
+        return(
+            <Navbar inverse collapseOnSelect>
+                <Navbar.Header>
+                    <Navbar.Brand
+                        className="menubar-brand">
+                        <a href="#">La caissière</a>
+                    </Navbar.Brand>
+                    {/* This is a specific tag to make the Navbar more 
+                      *  responsive 
+                      */}
+                    <Navbar.Toggle />
+                </Navbar.Header>
+                {/* This is also a tag to increase responsiveness */}
+            <Navbar.Collapse>
+                <Nav pullRight>
+                    <Navbar.Text className="menubar-time">
+                        {this.state.clock}
+                    </Navbar.Text>
+                    {/* Add link to the actual logout page */}
+                    <Button
+                        bsStyle="danger"
+                        href="#"
+                        className="menubar-logout navbar-btn"
+                    >Logout</Button>
+                </Nav>
+             </Navbar.Collapse>
+            </Navbar>
+        );
+    },
+});
+
+export default Menubar;
