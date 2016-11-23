7c0e3727bf5efd9c2b40c46dc8c78d242c76234f
Anthony Maton <maitre.negi@gmail.com>
2016-11-23 21:03:37 +0100

    Add User models and cleaned up permission unused field.
    We still need to choose what to do about those permission.

--- backend/models/models.js
+++ backend/models/models.js
@@ -11,6 +11,21 @@ const productSchema = new Schema({
     category: String,
 });
 
-let product = mongoose.model('Products', productSchema);
+const product = mongoose.model('Products', productSchema);
+
+const userSchema = new Schema({
+    pseudo: String,
+    password: String,
+    email: String,
+    firstname: String,
+    lastname: String,
+    lastConnection: Date,
+    accessCardId: Number,
+    // TODO : Think about wether or not to manage permissions
+});
+
+const user = mongoose.model('Users', userSchema);
 
 modules.exports.product = product;
+modules.exports.user = user;
+
