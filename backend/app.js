b8dd1f2cb20571e46522e142bd8c7948cc49bc52
Maton Anthony <MatonAnthony@users.noreply.github.com>
2016-11-24 12:42:25 +0100

    Fix configuration loading issues and mongoose connection (#26)

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,27 @@ const mongoose = require('mongoose');
 const index = require('./routes/index');
 const users = require('./routes/users');
 
+
+/**
+ * Load the config file
+ */
+let configPath;
+switch(process.env.NODE_ENV) {
+    case 'PRODUCTION': 
+        configPath = 'config/production.env';
+        break;
+    case 'DEV':  
+        configPath = 'config/dev.env';
+        break;
+    case 'TEST':  
+        configPath = 'config/test.env';
+        break;
+    default:  
+        /* Fallback to DEV config */
+        configPath = 'config/dev.env';
+}
+require('dotenv').config({silent: true, path: configPath});
+
 //This is temporary until the config file
 const DSN = 'https://0df4e05f1447440c8ec6c484ce86397d:1a5c162f9efa4913896636b12f561262@sentry.io/115501';
 
@@ -31,20 +52,19 @@ let logger = new winston.Logger({
 
 let app = express();
 
+
+mongoose.connect('mongodb://' + process.env.MONGO_USER + ':' + process.env.MONGO_PASS
+    + '@' + process.env.MONGO_URL);
+
 let db = mongoose.connection;
 db.on('error', (err) => {
     logger.log('error', '[DB] ' + err.name + ' : ' + err.message);
 });
 
-mongoose.connect(process.env.MONGO_USER + ':' + process.env.MONGO_PASS
-    + '@' + process.env.MONGO_URL);
-
 //The request handler must be the first item
 app.use(raven.middleware.express.requestHandler(DSN));
 //uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-//TODO: Fix
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
