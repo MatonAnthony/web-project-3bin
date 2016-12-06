fd5f484ad48a0ca3720be6fbdfb502e876c3b9dc
Delounea <martin_martin1993@hotmail.com>
2016-12-06 18:18:04 +0100

    Fix

--- backend/test/business/product.js
+++ backend/test/business/product.js
@@ -1,32 +1,11 @@
 const product = require('../../business/product');
-const models = require('../../business/models');
 const assert = require('assert');
-// Database configuration
 const mongoose = require('mongoose');
-
-const id = new mongoose.Types.ObjectId;
-
-describe('hook', function() {
-    before('Add a product to the DB', function() {
-        let productTemp = new models.product({
-            _id: id,
-            department: 'Alimentaire',
-            productName: 'Moules',
-            ean: '768501540',
-            price: 12,
-            tva: 3,
-            category: 'others',
-        });
-        productTemp.save().then().catch((error) => {
-            console.log(error);
-        });
-    });
-});
 
 describe('product', function() {
     describe('#register', function() {
         it('should add a product to the database', function() {
-            let futureProduct = {
+                let futureProduct = {
                 department: 'Alimentaire',
                 productName: 'Moules',
                 ean: '768501540',
@@ -43,17 +22,14 @@ describe('product', function() {
    describe('#getAllProducts', function() {
         it('should get all the products stored in the database', function() {
             product.getAllProducts().then(function(products) {
-                if(products[0]['department'] != 'Alimentaire' ||
-                    products[0]['productName'] != 'Moules' ||
-                    products[0]['ean'] != '768501540' ||
-                    products[0]['price'] != 12 ||
-                    products[0]['tva'] != 3 ||
-                    products[0]['category'] != 'others') {
-                        asser.fail();
-                }
-                assert.ok(products.length == 1);
+                assert.ok(products[0]['department'] == 'Alimentaire' &&
+                    products[0]['productName'] == 'Moules' &&
+                    products[0]['ean'] == '768501540' &&
+                    products[0]['price'] == 12 && products[0]['tva'] == 3 &&
+                    products[0]['category'] == 'others',
+                    'Les données récupérées ne sont pas valides');
             }).catch(function(error) {
-                assert.fail(error);
+                assert.ok(false, error);
             });
         });
    });
@@ -62,7 +38,7 @@ describe('product', function() {
 describe('product', function() {
     describe('#updateProduct', function() {
         it('should update the fields for the product of given id', function() {
-            return product.updateProduct(id,
+            return product.updateProduct(new mongoose.Types.ObjectId(123456789),
                 {price: 150, tva: 100});
         });
     });
@@ -71,7 +47,8 @@ describe('product', function() {
 describe('product', function() {
    describe('#removeProduct', function() {
          it('Should remove the product for the given id', function() {
-            return product.removeProduct(id);
+            return product.removeProduct(new mongoose.Types.ObjectId(123456789)
+            );
          });
    });
 });
