74261adccafa4af8b42e6a193ec930247f8543ef
Techy Martin <Delounea@users.noreply.github.com>
2016-12-08 09:02:29 +0100

    Add product logic and tests (#31)

---
+++ backend/business/product.js
@@ -0,0 +1,47 @@
+const models = require('./models.js');
+
+/**
+ * Register a product
+ * @param {Object} futureProduct The product to register to the database
+ * @return {Promise} return the future related to the registration process
+ */
+exports.register = (futureProduct) => {
+    let product = new models.product({
+        department: futureProduct.department,
+        productName: futureProduct.productName,
+        ean: futureProduct.ean,
+        price: futureProduct.price,
+        tva: futureProduct.tva,
+        category: futureProduct.category,
+    });
+    return product.save();
+};
+
+/**
+ * Get all products based on the fields of the param
+ * @param {Object} futureProduct An object with the fields to compare
+ * @return {Promise} return the future related to the getAllProducts process
+ */
+exports.getAllProducts = (futureProduct) => {
+    return models.product.find(futureProduct);
+};
+
+/**
+ * Update a product in the database
+ * @param {Object} productId The id of the product to update
+ * @param {Object} fieldsToUpdate An object with the fields with the new value
+ * @return {Promise} return the future related to the updateProduct process
+ */
+exports.updateProduct = (productId, fieldsToUpdate) => {
+    return models.product.findOneAndUpdate({_id: productId}, fieldsToUpdate,
+        {new: true});
+};
+
+/**
+ * Remove the product in the database, for the given id
+ * @param {Object} productId The id of the product to remove
+ * @return {Promise} return the future related to the remove process
+ */
+exports.removeProduct = (productId) => {
+    return models.product.find({_id: productId}).remove().exec();
+};
---
+++ backend/test/business/product.js
@@ -0,0 +1,55 @@
+const product = require('../../business/product');
+const assert = require('assert');
+const mongoose = require('mongoose');
+
+describe('product', function() {
+    describe('#register', function() {
+        it('should add a product to the database', function() {
+            let futureProduct = {
+                department: 'Alimentaire',
+                productName: 'Moules',
+                ean: '768501540',
+                price: 12,
+                tva: 3,
+                category: 'others',
+            };
+            return product.register(futureProduct);
+        });
+    });
+});
+
+describe('product', function() {
+    describe('#getAllProducts', function() {
+        it('should get all the products stored in the database', function() {
+            product.getAllProducts().then(function(products) {
+                assert.ok(products[0]['department'] == 'Alimentaire' &&
+                    products[0]['productName'] == 'Moules' &&
+                    products[0]['ean'] == '768501540' &&
+                    products[0]['price'] == 12 && products[0]['tva'] == 3 &&
+                    products[0]['category'] == 'others',
+                    'Invalid data');
+            }).catch(function(error) {
+                assert.ok(false, error);
+            });
+        });
+    });
+});
+
+describe('product', function() {
+    describe('#updateProduct', function() {
+        it('should update the fields for the product of given id', function() {
+            return product.updateProduct(new mongoose.Types.ObjectId(123456789),
+                {price: 150, tva: 100});
+        });
+    });
+});
+
+describe('product', function() {
+    describe('#removeProduct', function() {
+        it('Should remove the product for the given id', function() {
+            return product.removeProduct(new mongoose.Types.ObjectId(123456789)
+            );
+        });
+    });
+});
+
