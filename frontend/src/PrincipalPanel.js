17b2d7e4af74fc52aa95f2c9a513e93b3e6245bc
Techy Martin <Delounea@users.noreply.github.com>
2016-12-11 22:07:58 +0100

    Fix css of front-page (#43)

--- frontend/src/PrincipalPanel.js
+++ frontend/src/PrincipalPanel.js
@@ -5,6 +5,7 @@ import Ticket from './Ticket';
 import Menubar from './Menubar';
 import Auth from './Auth';
 import Login from './Login';
+import {Col, Grid, Row} from 'react-bootstrap';
 
 const PrincipalPanel = React.createClass({
     /*TODO retrieve categories and create buttons fom them*/
@@ -20,34 +21,46 @@ const PrincipalPanel = React.createClass({
 
     render() {
         let list = [];
-        list.push({name: 'Moules', price: '1.5'});
-        list.push({name: 'Frites', price: '1250'});
+        list.push({productName: 'Moules', price: '1.5'});
+        list.push({productName: 'Frites', price: '1250'});
         
         if(this.state.isLogged) {
         return (
             <div>
-                <Menubar callback={this.loggedOutCallback.bind(this)}/>
-                <Ticket className='col-xs-6 col-md-4' 
-                    ticketList={list} />
+                <Grid fluid>
+                    <Row>
+                        <Col md={12} xs={12}>
+                            <Menubar
+                                callback={this.loggedOutCallback.bind(this)}/>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md={4} xs={12}>
+                            <Ticket
+                                ticketList={list} />
+                        </Col>
+                        <Col md={8} xs={12}>
+                            <div >
+                                <CategoryPanel name='Moules'/>
+                                <CategoryPanel name='Bananes'/>
+                                <CategoryPanel name='Frites'/>
+                                <CategoryPanel name='Bièraubeurre'/>
+                                <CategoryPanel name='Dragées'/>
 
-                <div className='col-xs-12 col-sm-6 col-md-8'>
-                    <CategoryPanel name='Moules'/>
-                    <CategoryPanel name='Bananes'/>
-                    <CategoryPanel name='Frites'/>
-                    <CategoryPanel name='Bièraubeurre'/>
-                    <CategoryPanel name='Dragées'/>
-
-                    <CategoryPanel name='Dragées'/>
-                    <CategoryPanel name='Dragées'/>
-                    <CategoryPanel name='Dragées'/>
-                    <CategoryPanel name='Dragées'/>
-                    <CategoryPanel name='Dragées'/>
-                    <CategoryPanel name='Dragées'/>
-                    <CategoryPanel name='Dragées'/>
-                    <CategoryPanel name='Dragées'/>
-                    <CategoryPanel name='Dragées'/>
-                    <CategoryPanel name='Dragées'/>
-                </div>
+                                <CategoryPanel name='Dragées'/>
+                                <CategoryPanel name='Dragées'/>
+                                <CategoryPanel name='Dragées'/>
+                                <CategoryPanel name='Dragées'/>
+                                <CategoryPanel name='Dragées'/>
+                                <CategoryPanel name='Dragées'/>
+                                <CategoryPanel name='Dragées'/>
+                                <CategoryPanel name='Dragées'/>
+                                <CategoryPanel name='Dragées'/>
+                                <CategoryPanel name='Dragées'/>
+                            </div>
+                        </Col>
+                    </Row>
+                </Grid>
             </div>
         );
         } else {
